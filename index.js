b0f9a4d62436d0af604658f74a2fa558e54d65e4
sozibhossain <sozibhossain69@gmail.com>
2021-11-13 22:05:52 +0600

    added commit

---
+++ index.js
@@ -0,0 +1,180 @@
+const express = require('express')
+const app = express()
+const cors = require('cors');
+const admin = require("firebase-admin");
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+const port = process.env.PORT || 5000;
+
+
+
+
+// service account
+const serviceAccount = require('./autocar-ab3c5-firebase-adminsdk-4hxk0-1c3bca98ff.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xkmwi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function verifyToken (req, res, next){
+  if(req.headers?.authorization?.startsWith('Bearer ')){
+    const token = req.headers.authorization.split(' ')[1];
+
+
+    try{
+      const decodedUser = await admin.auth().verifyIdToken(token);
+      req.decodedEmail = decodedUser.email; 
+    }
+    catch{
+
+    }
+
+
+  }
+  next();
+}
+
+
+async function run(){
+    try{
+        await client.connect();
+        const database = client.db('auto_car');
+        const appointmentsCollection = database.collection('featuredcar');
+        const usersCollection = database.collection('users');
+        const productsCollection = database.collection('products');
+        const reviewsCollection = database.collection('review');
+
+
+        app.get('/products', async(req, res) =>{
+          const cursor = productsCollection.find({});
+          const products = await cursor.toArray();
+          res.send(products);
+        });
+        app.get('/products/:id', async(req, res) =>{
+          const id = req.params.id;
+          console.log('geting', id);
+          const query = {_id: ObjectId(id)};
+          const product = await productsCollection.findOne(query);
+          res.json(product)
+          
+        });
+
+        app.post('/review', async(req, res) => {
+          const review = req.body;
+          console.log('hit the', product);
+          const result = await reviewsCollection.insertOne(review);
+          console.log(result);
+          res.json(result)
+
+        });
+        app.post('/products', async(req, res) => {
+          const product = req.body;
+          console.log('hit the', product);
+          const result = await productsCollection.insertOne(product);
+          console.log(result);
+          res.json(result)
+
+
+        });
+
+        app.get('/featuredcar', verifyToken, async(req, res) => {
+          const email = req.query.email;
+          const query = {email: email}
+          console.log(query);
+          const cursor = appointmentsCollection.find(query);
+          const appointments = await cursor.toArray();
+          res.json(appointments);
+        })
+
+        app.post('/featuredcar', async(req, res) => {
+            const appointment = req.body;
+            const result = await appointmentsCollection.insertOne(appointment);
+            res.json(result)
+        });
+
+        app.get('/users/:email', async(req, res) => {
+          const email = req.params.email;
+          const query = {email: email};
+          const user = await usersCollection.findOne(query);
+          let isAdmin = false;
+          if(user?.role === 'admin'){
+            isAdmin= true;
+          }
+          res.json({admin: isAdmin});
+        })
+
+        app.post('/users', async(req, res) =>{
+          const user = req.body;
+          const result = await usersCollection.insertOne(user);
+          console.log(result);
+          res.json(result);
+        });
+
+        app.put('/users', async(req, res) => {
+          const user = req.body;
+          const filter = {email: user.email};
+          const options = { upsert: true };
+          const updateDoc = {$set: user};
+          const result = await usersCollection.updateOne(filter, updateDoc, options);
+          res.json(result);
+        });
+
+        app.put('/users/admin', verifyToken, async(req, res) => {
+          const user = req.body;
+          const requester = req.decodedEmail;
+          if(requester){
+            const requesterAccount = await usersCollection.findOne({email: requester});
+            if(requesterAccount.role === 'admin'){
+              const filter = {email: user.email};
+              const updateDoc = {$set: {role: 'admin'}};
+              const result = await usersCollection.updateOne(filter, updateDoc);
+              res.json(result);
+
+            }
+          }
+          else{
+            res.status(403).json({message: 'you do not have access to make admin'});
+          }
+
+          
+        })
+
+
+        // Delete API
+        app.delete('/products/:id', async(req, res) =>{
+          const id = req.params.id;
+          const query = {_id:ObjectId(id)};
+          const result = await productsCollection.deleteOne(query)
+          res.json(result);
+        });
+
+        app.delete('/featuredcar/:id', async(req, res) =>{
+          const id = req.params.id;
+          const query = {_id:ObjectId(id)};
+          const result = await appointmentsCollection.deleteOne(query)
+          res.json(result);
+        })
+    }
+    finally{
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Hello auto car!')
+})
+
+app.listen(port, () => {
+  console.log(`listening at ${port}`)
+})
